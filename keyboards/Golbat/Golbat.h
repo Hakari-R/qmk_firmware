121094569f7401f166d82d004b46f768cced2aca
Hakari <kikazu@outlook.com>
2019-10-11 18:48:27 +0800

    Try to support Golbat_V2 board with default and singal spacebar layer.

--- keyboards/Golbat/Golbat.h
+++ keyboards/Golbat/Golbat.h
@@ -26,7 +26,7 @@
  * The second converts the arguments into a two-dimensional array which
  * represents the switch matrix.
  */
-#define Golbat_V1( \
+#define default( \
     K00, K01, K02, K03, K04, K05, K06, K07, K08, K09, K0a, K0b, \
     K10, K11, K12, K13, K14, K15, K16, K17, K18, K19,      K1b, \
     K20,      K22, K23, K24, K25, K26, K27, K28, K29, K2a, K2b, \
@@ -36,4 +36,16 @@
     { K10, K11, K12, K13, K14, K15, K16, K17, K18, K19, ___, K1b }, \
     { K20, ___, K22, K23, K24, K25, K26, K27, K28, K29, K2a, K2b }, \
     { K30, K31, K32, ___, K34, ___, ___, K37, ___, K39, K3a, K3b } \
+}
+
+#define SingalSpace( \
+    K00, K01, K02, K03, K04, K05, K06, K07, K08, K09, K0a, K0b, \
+    K10, K11, K12, K13, K14, K15, K16, K17, K18, K19,      K1b, \
+    K20,      K22, K23, K24, K25, K26, K27, K28, K29, K2a, K2b, \
+    K30, K31, K32,                     K37,           K3a, K3b \
+) { \
+    { K00, K01, K02, K03, K04, K05, K06, K07, K08, K09, K0a, K0b }, \
+    { K10, K11, K12, K13, K14, K15, K16, K17, K18, K19, ___, K1b }, \
+    { K20, ___, K22, K23, K24, K25, K26, K27, K28, K29, K2a, K2b }, \
+    { K30, K31, K32, ___, ___, ___, ___, K37, ___, ___, K3a, K3b } \
 }
